e31581f6d9d99d1aeb47617d1a4f843165c1d51c
Vishal Singh <tmibvishal@gmail.com>
2018-12-23 11:39:43 +0530

    add comments and fix no extension searc

--- countFiles.sh
+++ countFiles.sh
@@ -4,6 +4,7 @@
 # in the given directory that
 # have the given file extension
 
+# verifying the arguments given
 if ! ([ $# -eq 1 ] || [ $# -eq 2 ]) ; then
     echo "usage: $0 directory extension" >&2;
     exit -1
@@ -12,13 +13,14 @@ fi
 directory=$1;
 extension=$2;
 
+# verifying if the first argument is a directory or not
 if [ ! -d $directory ]; then
     echo "$directory is not a directory" >&2
     exit -1
 fi
 
 if [ "$extension" = "" ]; then
-    echo "$(ls $directory | wc -l)"
+    echo "$(find $directory -maxdepth 1 -type f | wc -l)"
 else
     echo "$(find $directory -maxdepth 1 -name *$extension | wc -l)"
 fi
