e2a1ef27a813572ca6ae9c2ed2d27ef93e3936f7
Vishal Singh <tmibvishal@gmail.com>
2018-12-23 10:51:34 +0530

    verify the arguments

---
+++ countFiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Output the number of files
+# in the given directory that
+# have the given file extension
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 directory extension" >&2;
+    exit 1
+fi
+
+directory=$1;
+extension=$2;
+
+if [ ! -d $directory ]; then
+    echo "$directory is not a directory"
+fi
