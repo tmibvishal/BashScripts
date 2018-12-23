585a7af985b03cc442b33698ae24e9630ce4a8e5
Vishal Singh <tmibvishal@gmail.com>
2018-12-23 19:11:58 +0530

    check file and compare in dir1 & dir2

--- backup.sh
+++ backup.sh
@@ -15,5 +15,24 @@ elif [ ! -d $2 ]; then
     exit -1
 fi
 
-dir1=$1
-dir2=$2
+
+checkFiles1in2(){
+    #check file of $1 in $2
+    dir1=$1
+    dir2=$2
+    tempFile1="$(find $dir1 -type f | sort) "
+    for line in $tempFile1; do
+        # echo "${line#$dir1}"
+        text="${line#$dir1}"
+        if [ ! -e "$dir2$text" ]; then
+            echo "${line#$dir1}"
+        fi
+    done
+
+
+}
+
+echo "Files copied from /dir1 to /dir2 are:"
+checkFiles1in2 $1 $2
+echo "Files copied from /dir2 to /dir1 are:"
+checkFiles1in2 $2 $1
